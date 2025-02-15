184d622ba000768e6b5c07d9eb486fbdd1ca0e8d
faishalfhid <faishalfh1@gmail.com>
2025-02-15 22:53:25 +0700

    Update HTML

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const navList = document.querySelector(".nav-list");
   const navLinks = document.querySelectorAll(".nav-list li a"); // Ambil semua link dalam menu
 
-  // Toggle menu saat tombol hamburger diklik
   menuToggle.addEventListener("click", function () {
     navList.classList.toggle("show");
   });
 
-  // Tutup menu saat salah satu item navigasi diklik
   navLinks.forEach((link) => {
     link.addEventListener("click", function () {
       navList.classList.remove("show");
